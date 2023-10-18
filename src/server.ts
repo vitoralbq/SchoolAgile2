91b50928fa5a476fc2b0105264c040792b1d3117
Raniel Mendonça <raniel.mendonca@luizalabs.com>
2023-10-18 18:04:10 -0300

    adicionando env port

--- src/server.ts
+++ src/server.ts
@@ -6,19 +6,19 @@ import eventsRoutes from "./routes/eventsRoutes";
 dotenv.config();
 
 const server = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3002;
 
 server.use(cors());
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 
 server.get("/", (req, res) => {
-    res.send("FUNCIONANDO GRACAS A DEUS PAIS RECEBA!");
+  res.send("FUNCIONANDO GRACAS A DEUS PAIS RECEBA!");
 });
 
 // Defina suas rotas aqui usando participantsRouter
 server.use("/events", eventsRoutes);
 
 server.listen(port, () => {
-    console.log(`Servidor escutando na porta ${port}`);
+  console.log(`Servidor escutando na porta ${port}`);
 });
